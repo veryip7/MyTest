9149eb56ddd40882accb6819b84a5cf34f2c98e0
zhoubing <1510166998@qq.com>
2017-04-07 11:22:53 +0800

    成功吗

---
+++ ShuShu/ShuShu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShuShu
+//
+//  Created by zhoubing on 17/4/7.
+//  Copyright © 2017年 dev8. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ShuShu/ShuShu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ShuShu
+//
+//  Created by zhoubing on 17/4/7.
+//  Copyright © 2017年 dev8. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
